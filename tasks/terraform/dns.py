e36c35dd7da9bfc8177259335261a44727a448d0
James Fogarty <jayfo@users.noreply.github.com>
2021-12-01 05:43:25 -0800

    Pin IP for Development Transition

--- tasks/terraform/dns.py
+++ tasks/terraform/dns.py
@@ -16,8 +16,13 @@ ns = Collection('dns')
 # Define variables to provide to Terraform
 def terraform_variables_factory(*, context):
     with tasks.terraform.eip.eip_read_only(context=context) as eip:
+        # return {
+        #     'eip_public_ip': eip.output.public_ip
+        # }
+
+        # Temporarily pinned to existing aws-probe deployment in terraform_old
         return {
-            'eip_public_ip': eip.output.public_ip
+            'eip_public_ip': '35.153.174.68'
         }
 
 
