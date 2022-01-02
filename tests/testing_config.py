2e4f72c5f986323e77e563e268a86851f0c01605
James Fogarty <jayfo@users.noreply.github.com>
2022-01-01 17:03:57 -0800

    Introduce Demo Database

--- tests/testing_config.py
+++ tests/testing_config.py
@@ -3,31 +3,33 @@ import aws_infrastructure.tasks.ssh
 import scope.config
 import scope.testing.testing_config
 
+INSTANCE_SSH_CONFIG = aws_infrastructure.tasks.ssh.SSHConfig.load(
+    ssh_config_path="./secrets/configuration/instance_ssh.yaml",
+)
 DOCUMENTDB_CONFIG = scope.config.DocumentDBConfig.load(
     config_path="./secrets/configuration/documentdb.yaml",
 )
-
 DEMO_DATABASE_CONFIG = scope.config.DatabaseConfig.load(
     config_path="./secrets/configuration/demo_database.yaml",
 )
-
 DEV_DATABASE_CONFIG = scope.config.DatabaseConfig.load(
     config_path="./secrets/configuration/dev_database.yaml",
 )
 
-
-DEVELOPMENT_CONFIGS = {
-    "development_local": scope.testing.testing_config.TestingConfig(
-        instance_ssh_config=aws_infrastructure.tasks.ssh.SSHConfig.load(
-            ssh_config_path="./secrets/configuration/instance_ssh.yaml",
-        ),
+DATABASE_TESTING_CONFIGS = {
+    "database_dev": scope.testing.testing_config.TestingConfig(
+        instance_ssh_config=INSTANCE_SSH_CONFIG,
         documentdb_config=DOCUMENTDB_CONFIG,
         database_config=DEV_DATABASE_CONFIG,
         flask_config=None,
-    )
+    ),
+    "database_demo": scope.testing.testing_config.TestingConfig(
+        instance_ssh_config=INSTANCE_SSH_CONFIG,
+        documentdb_config=DOCUMENTDB_CONFIG,
+        database_config=DEMO_DATABASE_CONFIG,
+        flask_config=None,
+    ),
 }
-
-ALL_CONFIGS = DEVELOPMENT_CONFIGS
 
 UNIQUE_DOCUMENTDB_CONFIGS = [
     DOCUMENTDB_CONFIG,
