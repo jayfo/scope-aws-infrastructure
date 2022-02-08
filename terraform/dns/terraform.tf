a60d828818cfb92cc6fe6028d10667ad7f10a3fc
James Fogarty <jayfo@users.noreply.github.com>
2022-02-07 20:33:46 -0800

    App and Registry Subdomains

--- terraform/dns/terraform.tf
+++ terraform/dns/terraform.tf
@@ -16,14 +16,36 @@ module "hosted_zone" {
       name = "www.uwscope.org",
       ip = var.eip_public_ip,
     },
+
     /* Demo Deployment */
     {
       name = "demo.uwscope.org",
       ip = var.eip_public_ip,
     },
+    /* Demo Patient Deployment */
+    {
+      name = "app.demo.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    /* Demo Registry Deployment */
+    {
+      name = "registry.demo.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+
     /* Dev Deployment */
     {
       name = "dev.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    /* Dev Registry Deployment */
+    {
+      name = "registry.dev.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    /* Dev Patient Deployment */
+    {
+      name = "app.dev.uwscope.org",
       ip = var.eip_public_ip,
     },
   ]
