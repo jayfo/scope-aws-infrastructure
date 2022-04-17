e18d5cab08b889ee766cef4c413231ce75bc7d9a
James Fogarty <jayfo@users.noreply.github.com>
2022-04-17 15:21:23 -0700

    Version Updates with v0.2.0

--- docker/web_registry/codebuild_env.sh
+++ docker/web_registry/codebuild_env.sh
@@ -18,7 +18,8 @@ export GIT_REPOSITORY='https://github.com/uwscope/scope-web.git'
 # export GIT_REPOSITORY_BRANCH='freeze-2022-03-18b'
 # export GIT_REPOSITORY_BRANCH='freeze-2022-03-19'
 # export GIT_REPOSITORY_BRANCH='freeze-2022-03-20'
-export GIT_REPOSITORY_BRANCH='freeze-2022-03-21a'
+# export GIT_REPOSITORY_BRANCH='freeze-2022-03-21a'
+export GIT_REPOSITORY_BRANCH='release/v0.2.0'
 
 # Useful configuration for debugging
 # export GIT_REPOSITORY='https://github.com/jayfo/scope-web.git'
