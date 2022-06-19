08d8d55713812e2170a828265fb1a290bbde73eb
James Fogarty <jayfo@users.noreply.github.com>
2022-06-19 11:57:16 -0400

    Deployment of v0.4.0

--- tasks/codebuild/server_flask.py
+++ tasks/codebuild/server_flask.py
@@ -42,7 +42,8 @@ def codebuild_environment_variables_factory(*, context):
             # "REPOSITORY_TAGS": "freeze-2022-03-20 {}".format(BUILD_TIMESTAMP),
             # "REPOSITORY_TAGS": "freeze-2022-03-21a {}".format(BUILD_TIMESTAMP),
             # "REPOSITORY_TAGS": "v0.2.0 v0.2.0-{}".format(BUILD_TIMESTAMP),
-            "REPOSITORY_TAGS": "v0.3.0 v0.3.0-{}".format(BUILD_TIMESTAMP),
+            # "REPOSITORY_TAGS": "v0.3.0 v0.3.0-{}".format(BUILD_TIMESTAMP),
+            "REPOSITORY_TAGS": "v0.4.0 v0.4.0-{}".format(BUILD_TIMESTAMP),
         }
 
 
