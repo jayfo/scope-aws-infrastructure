f470fecf8ec653e3272b7a599e7b475f3ba0f1ae
James Fogarty <jayfo@users.noreply.github.com>
2024-03-22 07:41:08 -0700

    v13 Deployment Updates

--- docker/web_patient/codebuild_env.sh
+++ docker/web_patient/codebuild_env.sh
@@ -1,8 +1,9 @@
 # Environment variables for CodeBuild.
 export GIT_REPOSITORY='https://github.com/uwscope/scope-web.git'
+# export GIT_REPOSITORY_BRANCH='develop'
 # export GIT_REPOSITORY_BRANCH='release/v0.7.0-beta01'
 # export GIT_REPOSITORY_BRANCH='release/v0.11.0'
-export GIT_REPOSITORY_BRANCH='release/v0.12.0'
+export GIT_REPOSITORY_BRANCH='release/v0.13.0'
 
 # Useful configuration for debugging
 # export GIT_REPOSITORY='https://github.com/jayfo/scope-web.git'
