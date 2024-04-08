752b6d6cdc921630f31abf17b63a4904aee98f96
James Fogarty <jayfo@users.noreply.github.com>
2024-04-08 05:26:05 -0700

    Pin Terraform userpool

---
+++ terraform/userpool/userpool.tf
@@ -0,0 +1,37 @@
+resource "aws_cognito_user_pool" "userpool" {
+  name = "uwscope"
+
+  # Protect against deletion
+  lifecycle {
+    prevent_destroy = true
+  }
+  deletion_protection = "ACTIVE"
+
+  # Require a username,
+  # as allowing "email" or "phone_number" means those must be unique
+  alias_attributes = ["preferred_username"]
+
+  admin_create_user_config {
+    # Do not allow self signup, this ensures all accounts are consented participants
+    allow_admin_create_user_only = true
+  }
+
+  password_policy {
+    # Password generated at account creation is good for 90 days
+    temporary_password_validity_days = 90
+
+    # These are the defaults
+    minimum_length = 8
+    require_lowercase = true
+    require_numbers = true
+    require_symbols = true
+    require_uppercase = true
+  }
+}
+
+resource "aws_cognito_user_pool_client" "userpool_client" {
+  name = "uwscope_client"
+  user_pool_id = aws_cognito_user_pool.userpool.id
+
+  generate_secret = false
+}
