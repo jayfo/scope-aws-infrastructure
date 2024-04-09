5d03be188df1982bafec963cefe9c69cfc025c51
James Fogarty <jayfo@users.noreply.github.com>
2024-04-09 07:24:17 -0700

    Update SES Task Scaffold

--- tasks/terraform/ses.py
+++ tasks/terraform/ses.py
@@ -2,13 +2,26 @@ from aws_infrastructure.tasks import compose_collection
 import aws_infrastructure.tasks.library.terraform
 from collections import namedtuple
 from invoke import Collection
+from pathlib import Path
 
 from tasks.constants import TERRAFORM_BIN
+import tasks.terraform.dns
 
 CONFIG_KEY = 'ses'
 TERRAFORM_DIR = './terraform/ses'
+TERRAFORM_VARIABLES_PATH = Path(TERRAFORM_DIR, "variables.generated.tfvars")
 
 ns = Collection('ses')
+
+
+# Define variables to provide to Terraform
+def terraform_variables_factory(*, context):
+    with tasks.terraform.dns.dns_read_only(context=context) as dns_read_only:
+        hosted_zone_id = dns_read_only.output.hosted_zone_id
+
+    return {
+        'hosted_zone_id': hosted_zone_id,
+    }
 
 
 ns_userpool = aws_infrastructure.tasks.library.terraform.create_tasks(
@@ -16,6 +29,8 @@ ns_userpool = aws_infrastructure.tasks.library.terraform.create_tasks(
     terraform_bin=TERRAFORM_BIN,
     terraform_dir=TERRAFORM_DIR,
     auto_approve=False,
+    terraform_variables_factory=terraform_variables_factory,
+    terraform_variables_path=TERRAFORM_VARIABLES_PATH,
 )
 
 compose_collection(
