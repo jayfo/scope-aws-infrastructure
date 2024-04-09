ccef27e1578a21a74710dc0494c6264e60d09b60
James Fogarty <jayfo@users.noreply.github.com>
2024-04-09 04:13:33 -0700

    Rearrange Files to Configure Provider

---
+++ terraform/dns/dns.tf
@@ -0,0 +1,88 @@
+/*
+ * Zone with DNS records.
+ */
+module "hosted_zone" {
+  source = "github.com/fogies/aws-infrastructure//terraform_common/hosted_zone"
+
+  name = "uwscope.org"
+
+  address_records = [
+    /* Root Domains */
+    {
+      name = "uwscope.org",
+      ip = var.eip_public_ip,
+    },
+
+    /* Demo Deployment */
+    {
+      name = "demo.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "app.demo.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "registry.demo.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+
+    /* Dev Deployment */
+    {
+      name = "dev.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "registry.dev.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "app.dev.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+
+    /* FredHutch Deployment */
+    {
+      name = "fredhutch.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "app.fredhutch.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "registry.fredhutch.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+
+    /* MultiCare Deployment */
+    {
+      name = "multicare.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "app.multicare.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "registry.multicare.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+
+    /* SCCA Deployment */
+    {
+      name = "scca.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "app.scca.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+    {
+      name = "registry.scca.uwscope.org",
+      ip = var.eip_public_ip,
+    },
+  ]
+
+  tags = local.tags
+}
--- terraform/dns/terraform.tf
+++ terraform/dns/terraform.tf
@@ -1,88 +1,19 @@
 /*
- * Zone with DNS records.
+ * Pin specific versions.
  */
-module "hosted_zone" {
-  source = "github.com/fogies/aws-infrastructure//terraform_common/hosted_zone"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
 
-  name = "uwscope.org"
+  required_version = "1.7.4"
+}
 
-  address_records = [
-    /* Root Domains */
-    {
-      name = "uwscope.org",
-      ip = var.eip_public_ip,
-    },
-
-    /* Demo Deployment */
-    {
-      name = "demo.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "app.demo.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "registry.demo.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-
-    /* Dev Deployment */
-    {
-      name = "dev.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "registry.dev.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "app.dev.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-
-    /* FredHutch Deployment */
-    {
-      name = "fredhutch.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "app.fredhutch.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "registry.fredhutch.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-
-    /* MultiCare Deployment */
-    {
-      name = "multicare.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "app.multicare.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "registry.multicare.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-
-    /* SCCA Deployment */
-    {
-      name = "scca.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "app.scca.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-    {
-      name = "registry.scca.uwscope.org",
-      ip = var.eip_public_ip,
-    },
-  ]
-
-  tags = local.tags
+/*
+ * AWS configuration is provided by environment in surrounding task.
+ */
+provider "aws" {
 }
